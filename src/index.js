897fc3769b4df3025b1eebf75452920095fd9688
Harish <hp.onenonly@gmail.com>
2020-08-17 12:44:12 +0530

    still verbose

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useFormik } from "formik";
+import * as Yup from 'yup';
 import "./styles.css";
+import "./custom-styles.css";
 
-const validate = values => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = 'Required';
-  } else if (values.firstName.length > 15) {
-    errors.firstName = 'Must be 15 characters or less';
-  }
-
-  if (!values.lastName) {
-    errors.lastName = 'Required';
-  } else if (values.lastName.length > 20) {
-    errors.lastName = 'Must be 20 characters or less';
-  }
-
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
-  }
-
-  return errors;
-};
 
 const SignupForm = () => {
   const formik = useFormik({
@@ -33,7 +13,17 @@ const SignupForm = () => {
       lastName: '',
       email: '',
     },
-    validate,
+    validationSchema: Yup.object({
+       firstName: Yup.string()
+         .max(15, 'Must be 15 characters or less')
+         .required('Required'),
+       lastName: Yup.string()
+         .max(20, 'Must be 20 characters or less')
+         .required('Required'),
+       email: Yup.string()
+         .email('Invalid email address')
+         .required('Required'),
+     }),
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -50,7 +40,7 @@ const SignupForm = () => {
         value={formik.values.firstName}
       />
       {formik.touched.firstName && formik.errors.firstName ? (
-        <div>{formik.errors.firstName}</div>
+        <div className="red">{formik.errors.firstName}</div>
       ) : null}
       <label htmlFor="lastName">Last Name</label>
       <input
@@ -62,7 +52,7 @@ const SignupForm = () => {
         value={formik.values.lastName}
       />
       {formik.touched.lastName && formik.errors.lastName ? (
-        <div>{formik.errors.lastName}</div>
+        <div className="red">{formik.errors.lastName}</div>
       ) : null}
       <label htmlFor="email">Email Address</label>
       <input
@@ -74,9 +64,11 @@ const SignupForm = () => {
         value={formik.values.email}
       />
       {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
+        <div className="red">{formik.errors.email}</div>
       ) : null}
-      <button type="submit">Submit</button>
+      <p>
+        <button type="submit">Submit</button>
+      </p>
     </form>
   );
 };
